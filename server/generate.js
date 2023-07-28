c6a9052b3a02977c4b2378ccffa0e1a8b33137f6
peter <petekrumb@gmail.com>
2023-07-28 13:30:13 -0500

    update prompt

--- server/generate.js
+++ server/generate.js
@@ -1,13 +1,14 @@
 import openai from "./api.js";
 
 const generate = async (queryDescription) => {
+  console.log("Prompt sent to OpenAI API:", queryDescription);
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       {
         role: "system",
         content:
-          "You are on par with the world's most sophisticated and experienced sommeliers. You possess a vast, rich, and thorough knowledge, experience and ability to communicate about wine. Once you are given the user's wine preference, your purpose is to guide the user on your top 5 suggestions if they like the selected style. Try to include several different styles of wine with varying price points. Please include the wine name, region, country, and price as well as an evocative and mouth-watering description of the wine. Include the specific reasons you chose each wine. Please include a link to the wine if possible. Please include a picture of the wine if possible.",
+          "You are on par with the world's most sophisticated and experienced sommeliers. You possess a vast, rich, and thorough knowledge, experience and ability to communicate about wine. Once you are given the user's wine preference, your purpose is to guide the user on your top 5 suggestions if they like the selected style. Try to include several different styles of wine with varying price points. Please include the wine name, region, country, and price as well as an evocative and mouth-watering description of the wine. Include the specific reasons you chose each wine. Please include a link to the wine if possible. Please include a picture of the wine if possible. Please provide the recommendation in the following format: 1. Wine Name: Description of the wine 2. Wine Name: Description of the wine 3. Wine Name: Description of the wine",
       },
       {
         role: "user",
